9aac73da94306c230ebec61f4c556f26e78894c3
Ryan McUmber <ryanmma.rm@gmail.com>
2026-06-16 08:44:48 -0400

    Initial commit

---
+++ src/test/java/com/tennisapp/tennis_api/TennisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennisapp.tennis_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
